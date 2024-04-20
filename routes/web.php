ddd8c621d19954dbacc9f82690a5a01f0bbc076e
l37sg0 <petar_1v@abv.bg>
2024-04-20 13:40:48 +0300

    Api route resources - #2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\Api\ApplicantController;
+use App\Http\Controllers\Api\EducationController;
+use App\Http\Controllers\Api\SkillController;
+use App\Http\Controllers\Api\UniversityController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::group(['as' => 'api.', 'prefix' => 'api'], static function () {
+    Route::resource('applicants', ApplicantController::class);
+    Route::resource('educations', EducationController::class);
+    Route::resource('skills', SkillController::class);
+    Route::resource('universities', UniversityController::class);
 });
