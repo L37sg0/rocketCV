6c7472812d5bbbacb8223c0b0a585f51f084f104
l37sg0 <petar_1v@abv.bg>
2024-04-21 16:22:03 +0300

    Added fields in api resources - #3

--- app/Http/Resources/Applicant/ApplicantResource.php
+++ app/Http/Resources/Applicant/ApplicantResource.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Resources\Applicant;
 
+use App\Http\Resources\Education\EducationResource;
+use App\Http\Resources\Education\EducationResourceCollection;
+use App\Models\ApplicantInterface;
+use App\Models\Enums\Degree;
+use App\Models\Enums\Gender;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,6 +19,11 @@ class ApplicantResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        $data = parent::toArray($request);
+        $data['gender_name'] = Gender::from($data['gender'])->name;
+        /** @var ApplicantInterface $resource */
+        $resource = $this->resource;
+        $data['educations'] = EducationResource::collection($resource->getEducations());
+        return $data;
     }
 }
